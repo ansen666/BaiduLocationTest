08d8d8748933ff5237ad5aa38e127220aa34231e
anhui <privateworkemail@163.com>
2017-09-18 19:37:12 +0800

    更新

---
+++ app/src/main/java/com/ansen/baidumaptest/app/MyApplication.java
@@ -0,0 +1,14 @@
+package com.ansen.baidumaptest.app;
+
+import android.app.Application;
+
+/**
+ * @author ansen
+ * @create time 2017-09-18
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
